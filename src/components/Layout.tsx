7a366f20887d3f228d1ca794cadd89c116389121
lullabyX <hassan_rabbi@live.com>
2026-02-10 19:59:00 +0600

    oh boy oh boy it is better than I hooped

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -8,7 +8,7 @@ interface LayoutProps {
 
 export default function Layout({ children }: LayoutProps) {
   return (
-    <div className="flex flex-col h-screen w-screen bg-[#0a0a0a] text-white overflow-hidden">
+    <div className="flex flex-col h-full w-full bg-[#0a0a0a] text-white overflow-hidden">
       <div className="flex flex-1 overflow-hidden">
         <Sidebar />
         <div className="flex-1 flex flex-col overflow-hidden">{children}</div>
